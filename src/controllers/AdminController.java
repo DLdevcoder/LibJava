90ddb169cc5ef1e7f81ca4a574140c4c92f9293d
Nguyen Trung Kien <kienlx2005@gmail.com>
2024-10-23 23:13:11 +0700

    thay doi vi tri cac method member

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,182 +1,79 @@
 package controllers;
 import models.Admin;
-
-import utils.DatabaseConnection;
-
+import models.Member;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class AdminController {
-    // mai lam tiep
-    private List<Admin> Admins;
+    private Admin admin;
 
     public AdminController() {
-        Admins = new ArrayList<>();
+        this.admin = new Admin();
     }
-    public void getAdmins() {
-        String query = "SELECT * FROM admin";
-        List<Admin> Admins = new ArrayList<>();
-        // execute query
-        try (Connection conn = DatabaseConnection.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
-            while (rs.next()) {
-                int AdminId = rs.getInt("admin_id");
-                String name = rs.getString("name");
-                String address = rs.getString("address");
-                String phone = rs.getString("phone_number");
-                String email = rs.getString("email");
-                String password = rs.getString("password");
-                String createDate = rs.getString("created_at");
-                Admin Admin = new Admin(AdminId, name, address, phone, email, createDate, password);
-                Admins.add(Admin);
-            }
-            this.Admins = Admins;
-        } catch (SQLException e) {
+    public AdminController(Admin admin) {
+        this.admin = admin;
+    }
 
-            e.printStackTrace();
-        }
+    public List<Admin> getAdmins() {
+        return this.admin.getAdmins();
     }
 
     public void addAdmin() throws SQLException {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập tên thành viên: ");
-        String name = sc.nextLine();
-
-        System.out.print("Nhập địa chỉ: ");
-        String address = sc.nextLine();
-
-        System.out.print("Nhập số liên lạc: ");
-        String phone = sc.nextLine();
-
-        System.out.print("Nhập email: ");
-        String email = sc.nextLine();
-
-        System.out.print("Nhập mật khẩu: ");
-        String password = sc.nextLine();
-
-        try(Connection connection = DatabaseConnection.getConnection()){
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO admin (name, address, phone_number, email, password)  VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, name);
-            statement.setString(2, address);
-            statement.setString(3, phone);
-            statement.setString(4, email);
-            statement.setString(5, password);
-            int rowsAffected = statement.executeUpdate();
-            if(rowsAffected>0){
-                System.out.println("Thành viên mới đã được thêm thành công!.");
-            }
-            ResultSet resultSet = statement.getGeneratedKeys();
-            if(resultSet.next()){
-                int id = resultSet.getInt(1);
-                System.out.println("ID của thành viên mới: " + id);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        this.admin.addAdmin();
     }
 
     public void removeAdmin(int AdminId) throws SQLException {
-        String query = "DELETE FROM admin WHERE Admin_id = ?";
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setInt(1, AdminId);
-            int rowsAffected = stmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Admin removed successfully!");
-            } else {
-                System.out.println("Admin not found!");
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
+        this.admin.removeAdmin(AdminId);
     }
 
     public void updateAdmin(int adminId) {
-        String query = "UPDATE admin SET name = ?, address = ?, phone_number = ?, email = ?, password = ? WHERE admin_id = ?";
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập tên thành viên: ");
-        String name = sc.nextLine();
-
-        System.out.print("Nhập địa chỉ: ");
-        String address = sc.nextLine();
-
-        System.out.print("Nhập số liên lạc: ");
-        String phone = sc.nextLine();
-
-        System.out.print("Nhập email: ");
-        String email = sc.nextLine();
-
-        System.out.print("Nhập mật khẩu: ");
-        String password = sc.nextLine();
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setString(1, name);
-            stmt.setString(2, address);
-            stmt.setString(3, phone);
-            stmt.setString(4, email);
-            stmt.setString(5, password);
-            stmt.setInt(6, adminId);
-            int rowsAffected = stmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Admin updated successfully!");
-            } else {
-                System.out.println("Admin not found!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        this.admin.updateAdmin(adminId);
     }
 
     public void findAdmin() {
     }
 
-
-    // su dung cho admin
-    // xem xet
     public void displayAllAdmin() {
-        for (Admin Admin : Admins) {
-            System.out.println("Admin ID: " + Admin.getAdminId());
-            System.out.println("Name: " + Admin.getName());
-            System.out.println("Address: " + Admin.getAddress());
-            System.out.println("Phone: " + Admin.getPhone());
-            System.out.println("Email: " + Admin.getEmail());
-            System.out.println("Create Date: " + Admin.getCreateDate());
-            System.out.println("Password: " + Admin.getPassword());
-            System.out.println();
-        }
+        this.admin.displayAllAdmin();
 
     }
 
-    //su dung cho Admin
-    public void displayAdminInfo(int adminId) {
-        for (Admin Admin : Admins) {
-            if (Admin.getAdminId() == adminId) {
-                System.out.println("Admin ID: " + Admin.getAdminId());
-                System.out.println("Name: " + Admin.getName());
-                System.out.println("Address: " + Admin.getAddress());
-                System.out.println("Phone: " + Admin.getPhone());
-                System.out.println("Email: " + Admin.getEmail());
-                System.out.println("Create Date: " + Admin.getCreateDate());
-                return;
-            }
-        }
-        System.out.println("Admin not found!");
+    public void displayAdminInfo() {
+        this.admin.displayAdminInfo();
+    }
+
+    //member
+    public List<Member> getMembers() {
+        return this.admin.getMembers();
+    }
+    public void displayAllMember() {
+        this.admin.displayAllMember();
+    }
+    public void addMember () throws SQLException {
+        this.admin.addMember();
+    }
+
+    public void removeMember(int memberId) throws SQLException {
+        this.admin.removeMember(memberId);
+    }
+
+    public void updateMember(int memberId) {
+        this.admin.updateMember(memberId);
     }
 
 
 
-//
 //    public static void main(String[] agrs) throws SQLException {
-//        AdminController adminController = new AdminController();
+//        Admin admin = new Admin( 1,"name", "address", "phone", "email", "membershipDate", "password");
+//        AdminController adminController = new AdminController(admin);
+//        adminController.displayAdminInfo();
 //        //adminController.addAdmin();
-//        adminController.updateAdmin(1);
-//        adminController.getAdmins();
-//        adminController.displayAdminInfo(1);
+//        //adminController.updateAdmin(1);
+//        //adminController.getAdmins();
+//
+//
 //
 //    }
 
--- src/controllers/MemberController.java
+++ src/controllers/MemberController.java
@@ -1,172 +1,25 @@
 package controllers;
-import com.mysql.cj.protocol.Resultset;
 import models.Member;
-import utils.DatabaseConnection;
-
-import java.sql.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class MemberController {
-    private List<Member> members;
+    //private List<Member> members;
+    private Member member;
 
     public MemberController() {
-        members = new ArrayList<>();
     }
-    public void getMembers() {
-        String query = "SELECT * FROM members";
-        List<Member> members = new ArrayList<>();
-        // execute query
-        try (Connection conn = DatabaseConnection.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query);) {
-            while (rs.next()) {
-                int memberId = rs.getInt("member_id");
-                String name = rs.getString("name");
-                String address = rs.getString("address");
-                String phone = rs.getString("phone_number");
-                String email = rs.getString("email");
-                String password = rs.getString("password");
-                String membershipDate = rs.getString("membership_date");
-                Member member = new Member(memberId, name, address, phone, email, membershipDate, password);
-                members.add(member);
-            }
-            this.members = members;
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-        }
+    public MemberController(Member member) {
+        this.member = member;
     }
 
-    public void addMember() throws SQLException {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập tên thành viên: ");
-        String name = sc.nextLine();
-
-        System.out.print("Nhập địa chỉ: ");
-        String address = sc.nextLine();
-
-        System.out.print("Nhập số liên lạc: ");
-        String phone = sc.nextLine();
-
-        System.out.print("Nhập email: ");
-        String email = sc.nextLine();
-
-        System.out.print("Nhập mật khẩu: ");
-        String password = sc.nextLine();
-
-        try(Connection connection = DatabaseConnection.getConnection()){
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO members (name, address, phone_number, email, password)  VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, name);
-            statement.setString(2, address);
-            statement.setString(3, phone);
-            statement.setString(4, email);
-            statement.setString(5, password);
-            int rowsAffected = statement.executeUpdate();
-            if(rowsAffected>0){
-                System.out.println("Thành viên mới đã được thêm thành công!.");
-            }
-            ResultSet resultSet = statement.getGeneratedKeys();
-            if(resultSet.next()){
-                int id = resultSet.getInt(1);
-                System.out.println("ID của thành viên mới: " + id);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+    public void displayMemberInfo() {
+        this.member.displayMemberInfo();
     }
 
-    public void removeMember(int memberId) throws SQLException {
-        String query = "DELETE FROM members WHERE member_id = ?";
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setInt(1, memberId);
-            int rowsAffected = stmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Member removed successfully!");
-            } else {
-                System.out.println("Member not found!");
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void updateMember(int memberId) {
-        String query = "UPDATE members SET name = ?, address = ?, phone_number = ?, email = ?, password = ? WHERE member_id = ?";
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập tên thành viên: ");
-        String name = sc.nextLine();
-
-        System.out.print("Nhập địa chỉ: ");
-        String address = sc.nextLine();
-
-        System.out.print("Nhập số liên lạc: ");
-        String phone = sc.nextLine();
-
-        System.out.print("Nhập email: ");
-        String email = sc.nextLine();
-
-        System.out.print("Nhập mật khẩu: ");
-        String password = sc.nextLine();
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setString(1, name);
-            stmt.setString(2, address);
-            stmt.setString(3, phone);
-            stmt.setString(4, email);
-            stmt.setString(5, password);
-            stmt.setInt(6, memberId);
-            int rowsAffected = stmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Member updated successfully!");
-            } else {
-                System.out.println("Member not found!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void findMember() {
-    }
-
-
-    // su dung cho admin
-    // xem xet
-    public void displayAllMember() {
-        for (Member member : members) {
-            System.out.println("Member ID: " + member.getMemberId());
-            System.out.println("Name: " + member.getName());
-            System.out.println("Address: " + member.getAddress());
-            System.out.println("Phone: " + member.getPhone());
-            System.out.println("Email: " + member.getEmail());
-            System.out.println("Membership Date: " + member.getMembershipDate());
-            System.out.println("Password: " + member.getPassword());
-            System.out.println();
-        }
-
-    }
-
-    //su dung cho member
-    public void displayMemberInfo(int memberId) {
-        for (Member member : members) {
-            if (member.getMemberId() == memberId) {
-                System.out.println("Member ID: " + member.getMemberId());
-                System.out.println("Name: " + member.getName());
-                System.out.println("Address: " + member.getAddress());
-                System.out.println("Phone: " + member.getPhone());
-                System.out.println("Email: " + member.getEmail());
-                System.out.println("Membership Date: " + member.getMembershipDate());
-                return;
-            }
-        }
-        System.out.println("Member not found!");
-    }
+//    public static void main(String[] args) {
+//        Member member = new Member( 1,"name", "address", "phone", "email", "membershipDate", "password");
+//        MemberController memberController = new MemberController(member);
+//        memberController.displayMemberInfo();
+//    }
 
 
 }
